e5e9ec558692e2cd06665a16bc698f083e749964
SirDippingsauce <nobe5767@gmail.com>
2024-04-20 19:14:04 +0700

    msg

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Henlo World" << '\n';    
+    std::cout << "Henlo World 2" << '\n';    
+    std::cout << "Henlo World 3" << '\n';    
+    std::cout << "Henlo World 4" << '\n';    
+    std::cout << "Henlo World 5" << '\n';    
+    std::cout << "Henlo World 5" << '\n';    
+    std::cout << "Henlo World 7" << '\n';    
+}
